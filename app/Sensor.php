20d04c3344ee72a8e34b6ca032da4e1b917725e4
Leandro Martins Guimarães <leandropl@gmail.com>
2020-05-28 22:08:05 -0300

    Sensors DB structure

---
+++ app/Sensor.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sensor extends Model
+{
+    protected $fillable = ['users_id', 'lat', 'long'];
+}
---
+++ database/migrations/2020_05_29_002843_create_sensors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSensorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('lat', 9, 6);
+            $table->decimal('long', 9, 6);
+            $table->timestamps();
+
+            $table->foreignId('users_id')
+                    ->constrained('users')
+                    ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensors');
+    }
+}
